980bb4269f83ed67d4173bfdcff46b116e59e061
Reggie Baker <regbake@gmail.com>
2020-05-16 19:48:59 -0700

    begin adding pug templating

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const port = process.env.PORT || '8000';
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}`));
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-    res.status(200).send('Successful get at /');
+    res.render('index', { title: 'Home' })
+});
+
+app.get('/home', (req, res) => {
+    const test = 'test from index.js';
+    res.render('converted_from_html_lifeonreg_index', {
+        test
+    })
+});
+
+app.get('/user', (req, res) => {
+    res.render("user", {
+        title: "Profile",
+        userProfile: { nickname: "Naldo"}
+    });
 });
